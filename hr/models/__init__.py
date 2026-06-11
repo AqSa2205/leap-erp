86a2323881f4e0db3fc6426a6b9ba12013b302af
Aqsa Ahmed <aqsa2205@gmail.com>
2026-06-11 08:25:32 +0300

    HR: split models.py into hr/models/ package (no schema change)

---
+++ hr/models/__init__.py
@@ -0,0 +1,7 @@
+from .employee import Employee, EmployeeDocument
+from .assets import Asset, AssetAssignment, Vehicle
+
+__all__ = [
+    'Employee', 'EmployeeDocument',
+    'Asset', 'AssetAssignment', 'Vehicle',
+]
